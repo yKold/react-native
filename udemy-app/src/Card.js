9d78db57b37cd958bfa63e33498cbb5ac0d24bbf
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-29 20:41:38 -0300

    fas

--- udemy-app/src/Card.js
+++ udemy-app/src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+    const {name, email, id} = props;
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="robots" src="https://robohash.org/teste?200x200"/>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robots" src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
