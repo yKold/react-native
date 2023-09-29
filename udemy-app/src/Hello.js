ba7a2f511aa6dbe38485d890d38d2bbbb9368f01
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-28 21:04:35 -0300

    k

---
+++ udemy-app/src/Hello.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './Hello.css'
+
+class Hello extends Component {
+    render(){
+        return (
+            <div className='f1 tc'>
+                <h1>Hello World</h1>
+                <p>This is my first component!</p>
+            </div>
+        )
+    }
+}
+
+export default Hello
