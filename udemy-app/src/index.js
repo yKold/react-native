a4fce67d18bfdeebc810f4bdab43f60bf967815e
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-11-28 21:21:15 -0300

    njk

--- udemy-app/src/index.js
+++ udemy-app/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots'
+import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-  </React.StrictMode>
-);
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
