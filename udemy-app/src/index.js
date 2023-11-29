41a21bdc7f2366251d8317ddf7cf427ef31df60e
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-11-29 15:37:18 -0300

    v

--- udemy-app/src/index.js
+++ udemy-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './App.js';
+import App from './containers/App.js';
 
 ReactDOM.render(
   <App/>,
