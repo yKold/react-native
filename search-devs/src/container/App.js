13ec143d6283a528946d39173dcc9181d03b02da
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-11-30 14:50:36 -0300

    d

--- search-devs/src/container/App.js
+++ search-devs/src/container/App.js
@@ -8,30 +8,59 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            nome: [],
+            user: '',
+            objct: {},
+            login: '',
+            email: '',
+            location: '',
+            bio: '',
+            twitter_username: '',
+            followers: '',
+            following: '',
+            html_url: '',
         }
     }
+
     pegarValue = (event) => {
         console.log(event.target.value)
         this.setState({user: event.target.value})
+        fetch(`https://api.github.com/users/${this.state.user}`)
+          .then(response => response.json())
+          .then(data => {
+            this.setState({ 
+                objct: data, 
+                login: data.login, 
+                email: data.email, 
+                location: data.location,
+                bio: data.bio,
+                twitter_username: data.twitter_username,
+                followers: data.followers,
+                following: data.following,
+                html_url: data.html_url, 
+            }, () => {
+              console.log(this.state);
+            });
+          })
+          .catch(error => {
+            console.error('Erro ao buscar dados:', error);
+        });
     }
-// resolver
-    // componentDidMount(){
-    //     fetch(`https://api.github.com/users/${this.state.user}`)
-    //     .then(response=> response.json())
-    //     .then(data => this.setState({nome: data}))
-    // }
-
-    // click = () =>{
-    //     console.log(this.state.nome)
-    // }
 
     render(){
         return(
-            <div>
-                <input id="texto" type="text" placeholder="Type the username here..." onChange={this.pegarValue}/>
-                <button onClick={this.click}>aasdasds</button>
-                <Usuario />
+            <div className="container_principal">
+                <div>
+                    <input id="texto" type="text" placeholder="Type the username here..." onChange={this.pegarValue}/>
+                </div>
+                <Usuario
+                    login={this.state.user} 
+                    email={this.state.login} 
+                    location={this.state.location} 
+                    bio={this.state.bio} 
+                    twitter_username={this.state.twitter_username} 
+                    followers={this.state.followers} 
+                    following={this.state.following} 
+                    html_url={this.state.html_url}/>
             </div>
         )
     }
--- search-devs/src/container/usuario.js
+++ search-devs/src/container/usuario.js
@@ -1,11 +1,18 @@
 import React from "react";
+import './usuario.css'
 
 const Usuario = (props) => {
-    const {nome, login} = props
+    const {email, login, location, bio, twitter_username, followers, following, html_url} = props
     return(
-        <div>
-            <h1>{nome}</h1>
-            <h2>{login}</h2>
+        <div className="container">
+            <h1>login: {login}</h1>
+            <h2>Email: {email}</h2>
+            <h2>Location: {location}</h2>
+            <h2>Bio: {bio}</h2>
+            <h2>Twitter_username: {twitter_username}</h2>
+            <h2>Followers: {followers}</h2>
+            <h2>Following: {following}</h2>
+            <h2>Html_url: {html_url}</h2>
         </div>
     )
 }
