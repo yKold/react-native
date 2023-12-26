3e8f12651bb41d01da775236e513989c7171e729
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-12-25 22:00:20 -0300

    as

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react'
 import Nav from './components/nav/nav.js'
 import Logo from './components/logo/logo.js'
 import ImageLinkForm from './components/imageLink/imageLink.js'
+import Rank from './components/rank/rank.js'
 import './App.css';
+import Particle from './components/particles/Particles.jsx'
 
 class App extends Component{
   render(){
     return(
       <div className='app'>
+        <Particle/>
         <Nav/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
         {/* 
         
--- face-recognition/src/components/imageLink/imageLink.js
+++ face-recognition/src/components/imageLink/imageLink.js
@@ -1,13 +1,12 @@
 import React from "react";
+import "./imageLink.css"
 
 const ImageLinkForm = () => {
     return(
         <div>
-            <p className="f3">
-                {'this magic will detect faces in your pictures'}
-            </p>
+            <p className="f3"></p>
             <div className="center">
-                <div className="pa4 br3 shadow-5">
+                <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2 w-70 center" type="text"/>
                     <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
                 </div>
