10b6bbe28d311b18f41ed0d60bac3db3c3a3f192
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2024-01-02 19:42:56 -0300

    njm

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -2,11 +2,70 @@ import React, {Component} from 'react'
 import Nav from './components/nav/nav.js'
 import Logo from './components/logo/logo.js'
 import ImageLinkForm from './components/imageLink/imageLink.js'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import Rank from './components/rank/rank.js'
 import './App.css';
 import Particle from './components/particles/Particles.jsx'
 
+
 class App extends Component{
+
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    const PAT = 'c504ae0deea34cde8060909dc50abcda';
+    const USER_ID = 'ykold ';
+    const APP_ID = 'jotinha';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+    const IMAGE_URL = this.state.input;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      'a'
+    })
+    .catch(error => console.log('error', error));
+  }
+
   render(){
     return(
       <div className='app'>
@@ -14,10 +73,8 @@ class App extends Component{
         <Nav/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
-        {/* 
-        
-        <FaceRecognition/> */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     )
   }
